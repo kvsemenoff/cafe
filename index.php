1c72a4bea2d20f7286c3a545a519e8f23436b0ec
Konstantin Semenov <kvsemenoff@mail.ru>
2017-01-25 12:02:59 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+﻿
+	<?php require_once('includes/header.php'); ?>
+	<?php require_once('includes/header-top.php'); ?>
+	<?php require_once('includes/facts.php'); ?>
+	<?php require_once('includes/meet.php'); ?>
+	<?php require_once('includes/our.php'); ?>
+	<?php require_once('includes/biz.php'); ?>
+	<?php require_once('includes/gar.php'); ?>
+	<?php require_once('includes/what.php'); ?>
+	<?php require_once('includes/part.php'); ?>
+	<?php require_once('includes/write.php'); ?>
+	<?php require_once('includes/footer.php'); ?>
+
+	
