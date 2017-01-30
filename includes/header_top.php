cf003fadc3f955f219c84759ed4622cc03b62abb
Berzeg07 <berezg86@gmail.com>
2017-01-30 14:49:31 +0300

    29.14

--- includes/header_top.php
+++ includes/header_top.php
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 <section class="section">
 	<div class="main-container">
 		
-=======
+
 <section class="section section_padding section_db-head-top">
 	<div class="container">
 		 
@@ -15,6 +15,6 @@
 				<span class="db-phone">+375(29)3300000</span><br>
 				<a href="#" class="db-call">Заказать звонок</a>
 			
->>>>>>> 7b33f0345401dba607208806288073393fc0c885
+
 	</div>
 </section>
