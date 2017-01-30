ba064890cf24b06c17af034b7359f85a5e7dc173
Berzeg07 <berezg86@gmail.com>
2017-01-30 14:48:26 +0300

    29.14

--- includes/header_top.php
+++ includes/header_top.php
@@ -1,9 +1,5 @@
 <section class="section">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<h2></h2>
-			</div>
-		</div>
+	<div class="main-container">
+		
 	</div>
 </section>
