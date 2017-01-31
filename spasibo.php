65a1621dead49ea991438951d09f8bac84e4208e
Berzeg07 <berezg86@gmail.com>
2017-01-31 10:47:28 +0300

    29.14

--- spasibo.php
+++ spasibo.php
@@ -1,5 +1,5 @@
 	<?php require_once('includes/header.php'); ?>
-	<?php require_once('includes/header_top.php'); ?>
+	<?php require_once('includes/header_top2.php'); ?>
 	
 	<?php require_once('includes/footer.php'); ?>
 
