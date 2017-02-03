468bf2d367ebbdcc3c3d1bad6ba1d32501cbecb6
unknown <anzarsh@mail.ru>
2017-02-03 16:42:22 +0300

    add 1

--- includes/write.php
+++ includes/write.php
@@ -1,46 +1,48 @@
-<div id="contacts"></div>
-<section class="write">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="write__content clearfix">
-				    <div class="write__left">
-				            <h2 class=write__title>НАПИСАТЬ НАМ</h2>
-				            <p class="write__desc">
-				                У вас остались вопросы? Заполните форму ниже и мы обязательно вам ответим
-				            </p>
-				            <form action="#" class="form1 write__form">
-				                <input class="write__input-text" type="text" name="uname" placeholder="Имя*">
-				                <input class="write__input-text phone" type="text" name="phone" placeholder="Телефон*">
-				                <input class="write__input-text" type="text" name="email" placeholder="E-mail*">
-
-                                <textarea name="text" rows="10" placeholder="Сообщение*"></textarea>
-				                <input class="write__input-submit" type="submit" name="submit" name="form__submit" value="Отправить">
-
-                            
-				            </form>
-				    </div>
-				    <div class="write__right">
-				        <div class="right__container">
-				            <div class="write__phone">
-				                <div class="phone promo">Телефон</div>
-				                <p class="phone__number">+375(29)668-99-46</p>
-				                <p class="phone__number">+375(29)768-99-46</p>
-				            </div>
-				            <div class="write__email">
-				                <div class="email promo">E-mail</div>
-				                <p class="email__addr">wcafeby@gmail.com</p>
-				            </div>
-				            <div class="write__address">
-                                <div class="address promo">Адрес</div>
-				                <p class="address__text">г.Минск, ул. З.Бязули 11 БЦ Азимут</p>
-				            </div>
-				        </div>
-				    </div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-</div>
+<div class="section">
+    <div id="contacts"></div>
+    <section class="write">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="write__content clearfix">
+                        <div class="write__left">
+                                <h2 class=write__title>НАПИСАТЬ НАМ</h2>
+                                <p class="write__desc">
+                                    У вас остались вопросы? Заполните форму ниже и мы обязательно вам ответим
+                                </p>
+                                <form action="#" class="form1 write__form">
+                                    <input class="write__input-text" type="text" name="uname" placeholder="Имя*">
+                                    <input class="write__input-text phone" type="text" name="phone" placeholder="Телефон*">
+                                    <input class="write__input-text" type="text" name="email" placeholder="E-mail*">
+    
+                                    <textarea name="text" rows="10" placeholder="Сообщение*"></textarea>
+                                    <input class="write__input-submit" type="submit" name="submit" name="form__submit" value="Отправить">
+    
+                                
+                                </form>
+                        </div>
+                        <div class="write__right">
+                            <div class="right__container">
+                                <div class="write__phone">
+                                    <div class="phone promo">Телефон</div>
+                                    <p class="phone__number">+375(29)668-99-46</p>
+                                    <p class="phone__number">+375(29)768-99-46</p>
+                                </div>
+                                <div class="write__email">
+                                    <div class="email promo">E-mail</div>
+                                    <p class="email__addr">wcafeby@gmail.com</p>
+                                </div>
+                                <div class="write__address">
+                                    <div class="address promo">Адрес</div>
+                                    <p class="address__text">г.Минск, ул. З.Бязули 11 БЦ Азимут</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </div>
+    </div>
 </div>
