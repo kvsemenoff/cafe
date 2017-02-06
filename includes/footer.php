52aefe079cf8831e26c8270f932e658712da0798
engine77 <zaur.ka.00@mail.ru>
2017-02-06 12:24:16 +0300

    6

--- includes/footer.php
+++ includes/footer.php
@@ -9,16 +9,15 @@
 
 <div id="mask"></div>
 <div id="log-in" class="window">
-	<div class="dd-form-wrap">
-		<form class="form1" action="#" method="post">
-			<div class="form-wrap-main">
-				<div class="dd-close">Х</div>
-				<div class="dd-input-wrap">
-					<input class="" type="text" name="uname" placeholder="Ваше имя">
-					<input class="phone" type="text" placeholder="Введите телефон" name="phone">
-				</div>
-				<input type="submit" class="form-button" name="submit" value="Заказать звонок">
-			</div>
+	<div class="df-form-wrap">
+	 	<h2 class="df-cap-our dfpbottom">Написать <br> нам </h2>
+	 	<span class="df-poptxt2">У Вас остались вопросы? Заполните форму ниже и мы обязательно Вам ответим</span>
+		<form action="#" class="form1 write__form" method="post">
+			<div id="df-close" class="dd-close">Х</div>
+			<input class="write__input-text" type="text" name="uname" placeholder="Имя*" required>
+			<input class="write__input-text phone" type="text" name="phone" placeholder="Телефон*">
+			<input class="write__input-text" type="text" name="email" placeholder="E-mail*" required>
+			<input class="write__input-submit" type="submit" name="submit" name="form__submit" value="Отправить">			
 		</form>
 	</div>
 </div>
